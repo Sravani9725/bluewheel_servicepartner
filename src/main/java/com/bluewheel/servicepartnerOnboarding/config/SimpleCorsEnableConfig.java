bdda2c3b1a4ca8297876380ac6173743f1394e25
Sravani-Siramdas <ramya@zmater.com>
2024-10-05 22:23:15 +0530

    cors

---
+++ src/main/java/com/bluewheel/servicepartnerOnboarding/config/SimpleCorsEnableConfig.java
@@ -0,0 +1,36 @@
+package com.bluewheel.servicepartnerOnboarding.config;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SimpleCorsEnableConfig implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+
+		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH, OPTIONS");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "*");
+
+        chain.doFilter(req, res);
+ 
+
+	}
+
+}
